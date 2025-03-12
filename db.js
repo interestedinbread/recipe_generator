4eabd6b449565a1f56a85523148c713c02e4fb10
Eric Morrison <eric_m250@outlook.com>
2025-03-12 10:35:07 -0700

    updated db code to properly handle errors

--- db.js
+++ db.js
@@ -5,17 +5,19 @@ const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 })
 
-// db.connect((err) => {
-//     if(err) {
-//         console.error('Database connection failed:', err);
-//         return
-//     }
-//     console.log('Connected to mysql database')
-// });
-
-console.log('Connected to mysql db')
+db.getConnection((err, connection) => {
+    if (err) {
+        console.error('Database connection failed', err.message)
+    } else {
+        console.log('Connected to mysql db')
+        connection.release()
+    }
+})
 
 module.exports = db;
